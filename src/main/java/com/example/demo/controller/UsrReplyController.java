916952305eada9b3a4c2d36a8670b7552ec96753
indexl <indexl@naver.com>
2024-11-23 13:07:25 +0900

    mvcmvc

--- src/main/java/com/example/demo/controller/UsrReplyController.java
+++ src/main/java/com/example/demo/controller/UsrReplyController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,27 +9,44 @@ import com.example.demo.dto.Rq;
 import com.example.demo.service.ReplyService;
 import com.example.demo.util.Util;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class UsrReplyController {
 	
 	private ReplyService replyService;
-	private Rq rq;
-
 	
-	public UsrReplyController(ReplyService replyService, Rq rq) {
+	public UsrReplyController(ReplyService replyService) {
 		this.replyService = replyService;
-		this.rq = rq;
-	
 	}
 	
 	@PostMapping("/usr/reply/doWrite")
 	@ResponseBody
-	public String doWrite(int relId, String relTypeCode, String body) {
+	public String doWrite(HttpServletRequest req, String relTypeCode, int relId, String body) {
 		
-		replyService.replyWrite(rq.getLoginedMemberId(), relId, relTypeCode, body);
+		Rq rq = (Rq) req.getAttribute("rq");
 		
-		int id = replyService.getLastInsertId();
+		replyService.writeReply(rq.getLoginedMemberId(), relTypeCode, relId, body);
 		
-		return Util.jsReturn(String.format("%d번 댓글을 작성했습니다", id), String.format("../article/detail?id=%d", relId));			
-	}	
+		return Util.jsReturn("댓글 작성을 완료했습니다", String.format("../article/detail?id=%d", relId));
+	}
+	
+	@PostMapping("/usr/reply/doModify")
+	@ResponseBody
+	public String doModify(int id, int relId, String body) {
+		
+		replyService.modifyReply(id, body);
+		
+		return Util.jsReturn(String.format("%d번 댓글을 수정했습니다", id), String.format("../article/detail?id=%d", relId));
+	}
+
+	@GetMapping("/usr/reply/doDelete")
+	@ResponseBody
+	public String doDelete(int id, int relId) {
+		
+		replyService.deleteReply(id);
+		
+		return Util.jsReturn(String.format("%d번 댓글을 삭제했습니다", id), String.format("../article/detail?id=%d", relId));
+	}
+	
 }
--- src/main/java/com/example/demo/dao/ReplyDao.java
+++ src/main/java/com/example/demo/dao/ReplyDao.java
@@ -1,11 +1,17 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.demo.dto.Reply;
 
 @Mapper
-public interface ReplyDao{
+public interface ReplyDao {
 	
 	@Insert("""
 			INSERT INTO reply
@@ -16,11 +22,29 @@ public interface ReplyDao{
 					, relId = #{relId}
 					, `body` = #{body}
 			""")
-	public void replyWrite(int loginedMemberId, int relId, String relTypeCode, String body);
+	public void writeReply(int loginedMemberId, String relTypeCode, int relId, String body);
 
 	@Select("""
-			SELECT LAST_INSERT_ID()
+			SELECT r.*, m.loginId
+				FROM reply AS r
+				INNER JOIN `member` AS m
+				ON r.memberId = m.id
+				WHERE relTypeCode = #{relTypeCode}
+				AND relId = #{relId}
 			""")
-	public int getLastInsertId();
+	public List<Reply> getReplies(String relTypeCode, int relId);
 	
+	@Update("""
+			UPDATE reply
+				SET updateDate = NOW()
+					, `body` = #{body}
+				WHERE id = #{id}
+			""")
+	public void modifyReply(int id, String body);
+
+	@Delete("""
+			DELETE FROM reply
+				WHERE id = #{id}
+			""")
+	public void deleteReply(int id);
 }
